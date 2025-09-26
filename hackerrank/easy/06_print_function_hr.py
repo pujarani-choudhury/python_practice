9d27798aa7ad5797ca12e4bafcd451f609747315
pujarani-choudhury <pujac3010@gmail.com>
2025-09-26 07:49:58 +0530

    Refactor: use for loop instead of while loop for square calculation

--- hackerrank/easy/06_print_function_hr.py
+++
@@ -1,9 +0,0 @@
-#Read an integer n, from user. 
-# Without using any string method, print the following : 123....n. 
-# "...." represents the consecutive values in between.
-
-
-n = int(input())
-for i in range(1,n+1):
-    print(i,end='')
-    
