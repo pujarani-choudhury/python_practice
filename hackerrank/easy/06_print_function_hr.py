ab119a6794b2a941a45905c0033e545a0712684b
pujarani-choudhury <pujac3010@gmail.com>
2025-09-26 07:41:25 +0530

    Solved 'print function' problem  #Hackerrank 06 [Basic/Easy/Introduction]

---
+++ hackerrank/easy/06_print_function_hr.py
@@ -0,0 +1,9 @@
+#Read an integer n, from user. 
+# Without using any string method, print the following : 123....n. 
+# "...." represents the consecutive values in between.
+
+
+n = int(input())
+for i in range(1,n+1):
+    print(i,end='')
+    
